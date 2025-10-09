75101977475a4b9404c7e685a3ecec0b0e02a8a7
OUSMANE BALDE <baldemarc224@gmail.com>
2025-10-09 18:44:02 -0400

    fix: Configure CORS for Vercel deployment

--- Backend/src/main/java/com/qcm/generator/controller/HelloController.java
+++ Backend/src/main/java/com/qcm/generator/controller/HelloController.java
@@ -1,10 +1,12 @@
 package com.qcm.generator.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //Dit à Spring que cette classe gère des requêtes HTTP
 @RestController
+@CrossOrigin(origins = {"http://localhost:5173", "https://*.vercel.app"})
 public class HelloController {
 
     @GetMapping("/")
