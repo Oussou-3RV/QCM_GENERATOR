1886d681dbf5bc92449014da96b277ab263008da
OUSMANE BALDE <baldemarc224@gmail.com>
2025-11-07 23:42:02 -0500

    just improve some comment

--- Backend/src/main/java/com/qcm/generator/service/QcmService.java
+++ Backend/src/main/java/com/qcm/generator/service/QcmService.java
@@ -15,7 +15,7 @@ public class QcmService {
 
     @Autowired
     private OpenAIService openAIService;
-
+    //librairie JSON pour lire ,ecrire,convertir json en object
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     public QcmResponse generateQcm(String courseText) {
@@ -50,7 +50,7 @@ public class QcmService {
             // PARSING JSON
             // ========================================
 
-            // Parser la réponse JSON
+            // Parser la réponse JSON ou lire json sous forme d'arbre
             JsonNode rootNode = objectMapper.readTree(aiResponse);
             JsonNode questionsNode = rootNode.get("questions");
 
@@ -84,7 +84,7 @@ public class QcmService {
             System.err.println("Message : " + e.getMessage());
             e.printStackTrace();
 
-            // ❌ NE PAS utiliser le fallback( mancienne methode qui générait les qcm)
+            // ❌ NE PAS utiliser le fallback( ancienne methode qui générait les qcm)
             // À la place, lancer une exception avec un message clair
             throw new RuntimeException("L'intelligence artificielle est temporairement indisponible pour générer les questions. Veuillez réessayer plus tard ou contacter le propriétaire du site.");
         }
